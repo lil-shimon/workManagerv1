ff8b138b29db8abc3a0f554081e0a2f0f4f8b373
shimosawa <shimon101018@gmail.com>
2021-06-14 00:14:17 +0900

    delete cache

--- app/server.go
+++ app/server.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-    // "net/http"
     "github.com/labstack/echo"
     "github.com/lil-shimon/workManager/handler"
 )
 
 func main() {
     e := echo.New()
-    // e.GET("/", func(c echo.Context) error {
-        // return c.String(http.StatusOK, "Hello golang world")
-    // })
 
     // Routes
     e.POST("/", handler.CreateType)
